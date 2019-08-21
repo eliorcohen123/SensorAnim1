43f9befa3405a5a58a327d383813310739a5690e
EliorCohen <elior123456123123@gmail.com>
2019-08-21 12:17:29 +0300

    update

--- app/src/main/java/eliorcohen/com/sensoranim/ClassesPackage/MainActivity.java
+++ app/src/main/java/eliorcohen/com/sensoranim/ClassesPackage/MainActivity.java
@@ -2,15 +2,20 @@ package eliorcohen.com.sensoranim.ClassesPackage;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.view.View;
 import android.widget.Button;
+
+import com.google.firebase.analytics.FirebaseAnalytics;
 
 import eliorcohen.com.sensoranim.R;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button btnGame, btnShowScores;
+    private FirebaseAnalytics mFirebaseAnalytics;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,18 +24,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         initUI();
         initListeners();
+        getGoogleAnalytics();
     }
 
     private void initUI() {
         btnGame = findViewById(R.id.btnStartGame);
         btnShowScores = findViewById(R.id.btnShowScores);
 
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
         AppRater.app_launched(this);
     }
 
     private void initListeners() {
         btnGame.setOnClickListener(this);
         btnShowScores.setOnClickListener(this);
+    }
+
+    private void getGoogleAnalytics() {
+        Bundle bundle = new Bundle();
+        bundle.putString(FirebaseAnalytics.Param.ITEM_ID, "2");
+        bundle.putString(FirebaseAnalytics.Param.ITEM_NAME, "catchball");
+        bundle.putString(FirebaseAnalytics.Param.CONTENT_TYPE, "image");
+        mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle);
     }
 
     @Override
